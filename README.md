448d1cda392e540c2c80279dfe8eb14e914843f0
donotdisturb7 <renalddesire55@gmail.com>
2025-11-27 11:38:04 -0400

    update readme, add arg set stroke

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # fractal-rust
+
+Rénald DESIRE
+#     // pixel frame par frame 
+# // centre zoom, carré choisi au hazard, rand lib 
+#  // bareme : pt1 8; pt2 2 + 2 (bonus), pt3 4 + 4(bonus) 
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,36 @@
 use svg::Document;
 use svg::node::element::Path;
 use svg::node::element::path::Data;
+use arg::Args;
+use std::env;
+
+
+
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let fill_test = args.get(1).expect("Veuillez fournir un fichier en argument");
+
+    println!("{}", fill_test);
 
 
-let data = Data::new()
-    .move_to((10, 10))
-    .line_by((0, 50))
-    .line_by((50, 0))
-    .line_by((0, -50))
-    .close();
 
-let path = Path::new()
-    .set("fill", "none")
-    .set("stroke", "black")
-    .set("stroke-width", 3)
-    .set("d", data);
+    let data = Data::new()
+        .move_to((10, 10))
+        .line_by((0, 50))
+        .line_by((50, 0))
+        .line_by((0, -50))
+        .close();
 
-let document = Document::new()
-    .set("viewBox", (0, 0, 70, 70))
-    .add(path);
+    let path = Path::new()
+        .set("fill", "none")
+        .set("stroke", &**fill_test)
+        .set("stroke-width", 3)
+        .set("d", data);
 
-svg::save("image.svg", &document).unwrap();
+    let document = Document::new()
+        .set("viewBox", (0, 0, 70, 70))
+        .add(path);
+    
+    svg::save("image.svg", &document).unwrap();
 }
+
